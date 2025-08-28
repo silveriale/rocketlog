9424551d998a0480c21da7429b55555dbc66e5b6
silveriale <leticia.silveria.costa@gmail.com>
2025-08-28 15:37:19 -0300

    Adicionando Zod
    - npm i zod@3.23.8
    
    - como integrar a biblioteca Zod para validar parâmetros e requisições em nossa aplicação

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod";
 
 // Define um middleware para tratar erros na aplicação
 export function errorHandling(
@@ -13,6 +14,14 @@ export function errorHandling(
     return response.status(error.statusCode).json({ message: error.message }); // Retorna a resposta com o status e mensagem definidos pelo erro personalizado
   }
 
-  // Retorna um erro genérico (status 500) caso não seja um AppError
+  // Verifica se o erro veio da validação do Zod
+  if (error instanceof ZodError) {
+    return response.status(400).json({ // Retorna resposta com status 400 (erro de validação)
+      message: "validation error", // Define a mensagem de erro como "validation error"
+      issues: error.format(), // Retorna os detalhes formatados dos erros de validação do Zod
+    }); 
+  } 
+
+  // Retorna um erro genérico (status 500) caso não seja um AppError ou erro de validação 
   return response.status(500).json({ message: error.message });
 }

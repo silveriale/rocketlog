0902b560428ce85d6ae5d1dc395da8d09e1d6e26
silveriale <leticia.silveria.costa@gmail.com>
2025-09-03 16:07:46 -0300

    Cadastra Log
    - implementação um log de entrega em nosso sistema
    
     Comecei ajustando o status da entrega e, em seguida, utilizei a biblioteca Zod para validar os dados recebidos. Mostrei como buscar a entrega no banco de dados e realizar verificações para garantir que o pedido existe e já foi enviado antes de registrar qualquer movimentação

--- src/controlles/delivery-logs-controller.ts
+++ src/controlles/delivery-logs-controller.ts
@@ -1,9 +1,48 @@
-import { Request, Response } from "express";
+// Este arquivo define o controller responsável pela criação de logs de entrega, garantindo que apenas entregas válidas e com status adequado possam receber registros.
 
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+
+// Define a classe responsável pelos logs de entrega
 class DeliveryLogsController {
+  // Método assíncrono para criar um novo log de entrega
   async create(request: Request, response: Response) {
-    return response.json({ message: "ok" });
+    // Define o esquema de validação para o corpo da requisição usando zod
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(request.body); // Faz o parsing e validação do corpo da requisição conforme o esquema definido
+
+    // Busca a entrega no banco de dados pelo ID fornecido
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+    });
+
+    // Se a entrega não for encontrada, lança um erro 404
+    if (!delivery) {
+      throw new AppError("Entrega não encontrada!", 404);
+    }
+
+    // Se o status da entrega for "processing", impede a criação do log e lança um erro
+    if (delivery.status === "processing") {
+      throw new AppError("Mude o status para enviado (shipped)");
+    }
+
+    // Cria o log de entrega no banco de dados com o deliveryId e a descrição fornecidos
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+
+    return response.status(201).json(); // Retorna resposta de sucesso com status 201 (Created)
   }
 }
+
 
 export { DeliveryLogsController };

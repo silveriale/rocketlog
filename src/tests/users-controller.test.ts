258551b6b9860c234515d7f4e529251a82440cb9
silveriale <leticia.silveria.costa@gmail.com>
2025-09-04 17:24:35 -0300

    Testando Suite de Teste
    - npm i ts-node@10.9.2 -D
    
    - estruturar os testes usando describe e a adicionar scripts no package.json para facilitar a execução

---
+++ src/tests/users-controller.test.ts
@@ -0,0 +1,8 @@
+// Arquivo de testes unitários para o UsersController, que define um bloco de testes para o UsersController
+describe("UsersController", () => {
+  // Cria um caso de teste chamado "test"
+  it("test", () => {
+    // Exibe uma mensagem no console para indicar que o teste foi executado
+    console.log("Passou por aqui");
+  });
+});

a002ffe7cffd46dceaf83570f14ef80140511431
silveriale <leticia.silveria.costa@gmail.com>
2025-09-09 11:47:15 -0300

    Configurando a porta
    - como definir variáveis como a URL do banco de dados e a porta da aplicação, utilizando a biblioteca Zod para validação
    - isso garante flexibilidade, permitindo que mudemos a porta facilmente no ambiente de deploy sem alterar o código.

--- src/env.ts
+++ src/env.ts
@@ -4,13 +4,10 @@ import { z } from "zod";
 
 // Define um esquema de validação para as variáveis de ambiente esperadas
 const envSchema = z.object({
-  // Espera que DATABASE_URL seja uma string no formato de URL
-  DATABASE_URL: z.string().url(),
-  // Espera que JWT_SECRET seja uma string qualquer
-  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string().url(), // Espera que DATABASE_URL seja uma string no formato de URL
+  JWT_SECRET: z.string(), // Espera que JWT_SECRET seja uma string qualquer
+  PORT: z.coerce.number().default(3333), // Espera que PORT seja convertido para número, caso não esteja, e define 3333 como valor padrão
 });
 
 // Valida as variáveis de ambiente do processo atual com base no esquema definido
 export const env = envSchema.parse(process.env);
-
-
